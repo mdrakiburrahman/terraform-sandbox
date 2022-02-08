a57b2316f26f9fdb408752ed0c67dc3f09fa084f
Raki Rahman <mdrrahman@microsoft.com>
2022-02-08 07:55:36 -0500

    Implemented Delete

---
+++ Scripts/8_Delete.sh
@@ -0,0 +1,39 @@
+#######################################################
+# Setup
+#######################################################
+# Create new user
+curl -X POST docker_compose_api_1:9090/signup -d '{"username":"education", "password":"test123"}'
+
+#######################################################
+# Create 5 coffees
+#######################################################
+# Updated the build file with linux_amd64
+make install
+
+# Change to examples directory
+cd examples
+
+# Set env variables for auth
+export HASHICUPS_USERNAME=education
+export HASHICUPS_PASSWORD=test123
+
+# Debug enabled
+# export TF_LOG=TRACE
+
+# Init, plan and apply
+terraform init && terraform plan
+terraform apply --auto-approve
+
+#######################################################
+# Validate create from API
+#######################################################
+HASHICUPS_TOKEN=$(curl -X POST docker_compose_api_1:9090/signin -d '{"username":"education", "password":"test123"}' | jq .token | sed 's/\"//g')
+curl -X GET -H "Authorization: ${HASHICUPS_TOKEN}" docker_compose_api_1:9090/orders/1
+
+#######################################################
+# Delete via Terraform
+#######################################################
+terraform destroy --auto-approve
+
+# Validate delete from API
+curl -X GET -H "Authorization: ${HASHICUPS_TOKEN}" docker_compose_api_1:9090/orders/1
